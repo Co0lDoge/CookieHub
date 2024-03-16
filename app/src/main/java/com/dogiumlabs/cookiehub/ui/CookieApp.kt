3c1408b83761cb877fd6afadebeabdf6e25fec1b
CoolDoge <thedarkguard60@gmail.com>
2024-03-16 16:36:04 +0500

    Restore CookieApp's original name (accidentally changed)

--- app/src/main/java/com/dogiumlabs/cookiehub/ui/CookieApp.kt
+++ app/src/main/java/com/dogiumlabs/cookiehub/ui/CookieApp.kt
@@ -4,9 +4,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -16,7 +13,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
