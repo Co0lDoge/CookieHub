a95eac54c0ef67251caed52569f665770fdbd69d
CoolDoge <thedarkguard60@gmail.com>
2024-03-29 13:02:06 +0500

    Replace column with lazycolumn to improve loading time

--- app/src/main/java/com/dogiumlabs/cookiehub/ui/components/ListScreen.kt
+++ app/src/main/java/com/dogiumlabs/cookiehub/ui/components/ListScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -35,12 +37,8 @@ fun ListScreen(
     onItemClick: (Cookie) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .verticalScroll(rememberScrollState())
-            .fillMaxSize()
-    ) {
-        cookiesList.forEach { cookie ->
+    LazyColumn(modifier = modifier) {
+        items(cookiesList) {cookie ->
             CookieCard(
                 cookie = cookie,
                 onClick = onItemClick
